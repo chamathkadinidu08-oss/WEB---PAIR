705c15669b07c6e83661ae6587fc29e995cd6382
chamathkadinidu08-oss <chamathkadinidu08@gmail.com>
2026-08-25 18:24:10 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -3,18 +3,18 @@ const config = require('../config');
 
 cmd({
     pattern: "alive",
-    desc: "Check bot online or no.",
+    desc: "හායි ලස්සන ලමයෝ🙈.",
     category: "main",
     filename: __filename
 },
-async (danuwa, mek, m, {
+async (Panther, mek, m, {
     from, quoted, body, isCmd, command, args, q, isGroup,
     sender, senderNumber, botNumber2, botNumber, pushname,
     isMe, isOwner, groupMetadata, groupName, participants,
     groupAdmins, isBotAdmins, isAdmins, reply
 }) => {
     try {
-        return await danuwa.sendMessage(from, {
+        return await Panther.sendMessage(from, {
             image: { url: config.ALIVE_IMG },
             caption: config.ALIVE_MSG
         }, { quoted: mek });
